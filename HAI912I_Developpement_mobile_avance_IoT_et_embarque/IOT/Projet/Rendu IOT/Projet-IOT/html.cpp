a390398cd69979d2f579733c918b4217ddddebdb
matthieu trinquart <matthieu.trinquart@etu.umontpellier.fr>
2023-02-14 18:41:00 +0100

    dernier push !

---
+++ HAI912I_Developpement_mobile_avance_IoT_et_embarque/IOT/Projet/Rendu IOT/Projet-IOT/html.cpp
@@ -0,0 +1,7 @@
+#include "html.h"
+String HTML = "<a href=\"/\"> Racine du serveur </a> <br><br>"
+                "<a href=\"/readTemperature\"> Lecture de la température </a> <br><br>"
+                "<a href=\"/readLumiere\"> Lecture de la lumière </a> <br><br>"
+                "<a href=\"/led/on\"> Allumer la led </a><br><br>"
+                "<a href=\"/led/off\"> Eteindre la led  </a> <br><br>"
+                "<a href=\"/led/auto\"> Mettre la led en mode AUTO </a><br><br>";
